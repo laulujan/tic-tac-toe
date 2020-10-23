c1ad3ad42492193018a30b34ec34702d294f39ec
Laura Lujan <lauralujanhdez@gmail.com>
2020-10-23 18:24:41 -0500

    started modules

---
+++ app.js
@@ -0,0 +1,25 @@
+const gameBoard = (function(){
+    const board = ["", "", "",
+                   "", "", "",
+                   "", "", ""];
+    
+    let isOver = false;
+
+    const pickCell = function(mark, index){
+
+    }
+
+    
+
+          
+})();
+
+const player = (function(name, mark){
+    
+
+})();
+
+const displayController = (function(){
+    const gameBoard = document.getElementsByClassName('grid');
+    gameBoard.addEventListener('click', function(){})
+})
